fb4b6ddba482165d72c63f7ae8f25afcfada23f6
Marius Racovita <racovita67@gmail.com>
2025-10-11 21:14:02 +0300

    Add installer to generate executable

---
+++ installer/pyinstaller_main.py
@@ -0,0 +1,18 @@
+
+import PyInstaller.__main__
+import os
+
+if __name__ == "__main__":
+    # Start main application
+    script_dn = os.path.dirname(os.path.abspath(__file__))
+    main_dn = script_dn[0:script_dn.rindex("\\")]
+    main_fn = os.path.join(main_dn, "main.py")
+
+    PyInstaller.__main__.run([
+    main_fn,
+    '--onefile',
+    '--windowed',
+    '--icon=installer\\icon.ico',
+    ])
+
+
